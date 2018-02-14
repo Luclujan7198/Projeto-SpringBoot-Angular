bfb3dff313f77dc392fa87c56c93faf1dcfd572e
resource205 (Raquel Bazanelli) <resource205@fornecedor.gamasaude.com.br>
2018-02-14 16:35:53 -0200

    ok

--- src/main/java/com/example/algamoney/api/config/BasicSecurityConfig.java
+++ src/main/java/com/example/algamoney/api/config/BasicSecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.algamoney.api.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Profile("basic-security")
 @EnableWebSecurity
