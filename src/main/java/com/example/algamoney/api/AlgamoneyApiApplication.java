23f1b636bfe51e1a6021b19a659499a373714127
resource205 (Raquel Bazanelli) <resource205@fornecedor.gamasaude.com.br>
2018-02-15 09:48:21 -0200

    teste deploy

--- src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages={
 "com.example.algamoney.api.cors.CorsFilter", "com.example.algamoney.api.config.property.AlgamoneyApiProperty"})
-
 public class AlgamoneyApiApplication {
 
 	public static void main(String[] args) {
