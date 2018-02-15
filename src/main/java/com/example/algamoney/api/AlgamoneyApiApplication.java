9886589b1d6b6b0f8dbde2c7b786c38bb9c13da4
resource205 (Raquel Bazanelli) <resource205@fornecedor.gamasaude.com.br>
2018-02-15 13:59:10 -0200

    token correto, run application funcionando

--- src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
@@ -2,9 +2,12 @@ package com.example.algamoney.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(scanBasePackages={
-"com.example.algamoney.api.cors.CorsFilter", "com.example.algamoney.api.config.property.AlgamoneyApiProperty"})
+import com.example.algamoney.api.config.property.AlgamoneyApiProperty;
+
+@SpringBootApplication
+@EnableConfigurationProperties(AlgamoneyApiProperty.class)
 public class AlgamoneyApiApplication {
 
 	public static void main(String[] args) {
